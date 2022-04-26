fe1779b918b6cdc0749472ae9b30e7b0b773ba99
CHIINGTING <tad20077@gmail.com>
2022-04-26 14:48:26 +0800

    add

--- link-list/remove-linked-list-elements/remove-linked-list-elements.c
+++ link-list/remove-linked-list-elements/remove-linked-list-elements.c
@@ -10,13 +10,12 @@ struct ListNode* removeElements(struct ListNode* head, int val){
     if(!*current){
         return NULL;
     }
-    while(*current  &&(*current)->val == val){
-        *current = (*current) -> next;
-    }
-    
-    while((*current) && (*current)->next ){
-        while(((*current)->next)&&((*current)->next->val == val)){
-            (*current) -> next = (*current) -> next -> next;
+    while(*current){
+        while((*current)->val == val){
+            *current = (*current) -> next;
+            if(!*current){
+                return head;
+            }
         }
         current = &(*current)->next;
     }
