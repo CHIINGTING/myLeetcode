39a86830c113b5b31b8d2046c46e167e2ea2fa17
CHIINGTING <tad20077@gmail.com>
2024-11-19 01:53:14 +0800

    feat(): add Search in Rotated Sorted Array

--- arrays/search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ arrays/search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,29 +1,26 @@
 class Solution(object):
     def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
         left, right = 0, len(nums) - 1
+
         while left <= right:
             mid = (left + right) // 2
-            # 如果找到目標值
+
+            # 找到目標值
             if nums[mid] == target:
                 return mid
-            # 判斷左半部分是否有序
-        if nums[left] <= nums[mid]:
-            # 如果 target 在左半部分範圍內
-            if nums[left] <= target < nums[mid]:
-                right = mid - 1
+
+            # 判斷左半區間是否有序
+            if nums[left] <= nums[mid]:
+                # 目標值在左半區間內
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:  # 否則在右半區間
+                    left = mid + 1
             else:
-                left = mid + 1
-        # 否則右半部分有序
-        else:
-            # 如果 target 在右半部分範圍內
-            if nums[mid] < target <= nums[right]:
-                left = mid + 1
-            else:
-                right = mid - 1
+                # 右半區間有序
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:  # 否則在左半區間
+                    right = mid - 1
         return -1
 
